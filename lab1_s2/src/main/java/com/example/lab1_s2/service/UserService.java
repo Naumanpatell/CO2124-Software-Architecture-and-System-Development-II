ddbaed315d9ef8f2cfe5ee1b4d83d58fe4bffa4c
Nauman patel <nauman0504@gmail.com>
2026-01-22 17:37:43 +0000

    Lab-1, Task-2

--- lab1_s2/src/main/java/com/example/lab1_s2/service/UserService.java
+++ lab1_s2/src/main/java/com/example/lab1_s2/service/UserService.java
@@ -1,13 +1,32 @@
 package com.example.lab1_s2.service;
 
+import com.example.lab1_s2.model.User;
 import com.example.lab1_s2.repo.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository,
+                       PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
+    public void registerUser(String username, String password) {
+
+        User user = new User();
+        user.setUsername(username);
+
+        String encodedPassword = passwordEncoder.encode(password);
+        user.setPassword(encodedPassword);
+
+        userRepository.save(user);
+    }
+}
