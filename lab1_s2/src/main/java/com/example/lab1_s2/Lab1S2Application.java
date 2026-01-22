2fafbdbe888489a2c057401e8809f59d73e7bd12
Nauman patel <nauman0504@gmail.com>
2026-01-22 17:37:43 +0000

    Lab-1, Task-1

---
+++ lab1_s2/src/main/java/com/example/lab1_s2/Lab1S2Application.java
@@ -0,0 +1,13 @@
+package com.example.lab1_s2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1S2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab1S2Application.class, args);
+    }
+
+}
---
+++ lab1_s2/src/main/java/com/example/lab1_s2/repo/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.lab1_s2.repo;
+
+import com.example.lab1_s2.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
